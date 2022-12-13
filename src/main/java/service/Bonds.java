f1d7f6699bd49973cbf708d09b30fc1321c42055
sourav <souravsengupta20192019@gmail.com>
2022-12-13 18:51:51 +0530

    BFS-6

--- src/main/java/service/Bonds.java
+++ src/main/java/service/Bonds.java
@@ -5,17 +5,5 @@ public class Bonds {
 	private void bond_msg1(){
 		System.out.println("Bonds");
 	}
-	
-	private void bond_msg2(){
-		System.out.println("Bonds-2");
-	}
-	
-	private void bond_msg3(){
-		System.out.println("Bonds-3");
-	}
-	
-	private void bond_msg5(){
-		System.out.println("Bonds-5");
-	}
 
 }
