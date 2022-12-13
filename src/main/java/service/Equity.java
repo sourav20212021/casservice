14ffdcccd0c10c9df3110bd6382926ed86a1ec09
sourav <souravsengupta20192019@gmail.com>
2022-12-13 18:59:18 +0530

    com-2

--- src/main/java/service/Equity.java
+++ src/main/java/service/Equity.java
@@ -5,4 +5,10 @@ public class Equity {
 	public void equity_msg_1(){
 		System.out.println("Equity msg-1");
 	}
+	public void equity_msg_2(){
+		System.out.println("Equity msg-2");
+	}
+	public void equity_msg_3(){
+		System.out.println("Equity msg-3");
+	}
 }
