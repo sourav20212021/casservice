c4cd51e484d715262d08275c6e60f70be3914a7f
sourav20212021 <82802288+sourav20212021@users.noreply.github.com>
2022-12-16 15:16:16 +0530

    Update Stocks.java
    comm-25

--- src/main/java/service/Stocks.java
+++ src/main/java/service/Stocks.java
@@ -16,7 +16,7 @@ public class Stocks {
 	public void stock_4(){
 		System.out.println("Stock-4");
 	}
-	public void stock_5(){
-		System.out.println("Stock-5");
+	public void stock_6(){
+		System.out.println("Stock-6");
 	}
 }
