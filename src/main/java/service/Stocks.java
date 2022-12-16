43b498365e2335d35db5e04f0a178e47c94aa815
sourav <souravsengupta20192019@gmail.com>
2022-12-16 15:44:01 +0530

    commit-26

--- src/main/java/service/Stocks.java
+++ src/main/java/service/Stocks.java
@@ -13,6 +13,9 @@ public class Stocks {
 	public void stock_3(){
 		System.out.println("Stock-3");
 	}
+	public void stock_5(){
+		System.out.println("Stock-3");
+	}
 	public void stock_4(){
 		System.out.println("Stock-3");
 	}
